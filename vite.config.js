c4857ea740e06a552bf3bb3982ab2de1eb494caf
Diego REy <reydiegogo026gmail.com>
2025-02-06 11:37:29 -0300

    Aregglo5 Vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
   build: {
     outDir: "dist",
-    chunkSizeWarningLimit: 3000, // Aumenta el límite
+    chunkSizeWarningLimit: 3000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react"; // React en su propio chunk
+            if (id.includes("recharts")) return "recharts"; // lodash en su propio chunk
+            if (id.includes("@react-pdf")) return "@react-pdf"; // moment.js en su propio chunk
+            if (id.includes("@leaflet-src.js")) return "@leaflet-src.js"; // moment.js en su propio chunk
             return "vendor";
           }
         },
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "stats.html", // Nombre correcto del archivo
+      template: "treemap", // Usa el formato visual de árbol
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 });
